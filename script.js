3f495f821fd7f2617e10a1a49ddc160015bf44d4
divyaramesh34 <divyasri2364@gmail.com>
2025-04-25 20:30:52 +0530

    firstCommit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+// 1. Grade Calculator
+var num = prompt("EnterMark")
+    if (num >= 90) {
+        alert("A Grade")
+    }
+    else if (num >= 80) {
+        alert("B Grade")
+    }
+    else if (num >= 70) {
+        alert("C Grade")
+    }
+    else if (num >= 60) {
+        alert("D Grade")
+    }
+    else if (num >= 50) {
+        alert("E Grade")
+    }
+    else {
+        alert("F Grade")
+     }
+
+
